e08c42af7f4fe9d31ae667d957210ef574170261
PeterOnCode <peteroncode@gmail.com>
2022-05-08 12:32:27 +0200

    [3-blog-posts] 3.1 - Rendering Components in MDX

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,23 @@ module.exports = {
     description:
       'A site we built together doing a full-day Frontend MAsters Gatsby workshop!',
   },
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
+    // {
+    //   resolve: 'gatsby-source-filesystem',
+    //   options: {
+    //     name: 'posts',
+    //     path: 'posts',
+    //   },
+    // },
+  ],
 }
