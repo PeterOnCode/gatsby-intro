d34910e2bf67c4ccb3ecaca1773a17c8953bab68
PeterOnCode <peteroncode@gmail.com>
2022-05-14 12:50:55 +0200

    [5-working-with-images] 5.1. - Adding a Hero Image Box

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,21 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import usePosts from '../hooks/use-posts'
 import PostPreview from '../components/post-preview'
+import Hero from '../components/hero'
 
 const Index = () => {
   const posts = usePosts()
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p>Hello Budapest!</p>
-      <Link to="/about/">Learn about Me &rarr;></Link>
-
-      <h2>Read my blog</h2>
-      {posts.map((post) => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h2>Read my blog</h2>
+        {posts.map((post) => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </>
   )
 }
 export default Index
